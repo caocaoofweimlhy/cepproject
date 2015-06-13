81458f563e813c8a0dca13edabb7ab9e7d4e1e38
caocaoofweimlhy <caocaoofweimlhy@gmail.com>
2015-06-13 17:54:48 +0800

    Removed old files

--- src/resource.js
+++ src/resource.js
@@ -1,21 +1,7 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
     CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png",
-    jumpright_png: "res/jumpright1.png",
-    jumpright_plist: "res/jumpright.plist",
-    jumpleft_png: "res/jumpleft1.png",
-    jumpleft_plist: "res/jumpleft.plist",
-    walk_png:"res/walkright1.png",
-    walk_plist:"res/walkright.plist",
-    background_png:"res/background.png", 
-    token_png: "res/coin1.png",
-    token_plist:"res/coin.plist",
-    player_png : "res/walkright1.png",
-    enemy_png : "res/invader.png",
-    ground_png: "res/ground.png",
-    platform_png: "res/platform.png",
-    wall_png: "res/wall.png"
+    CloseSelected_png : "res/CloseSelected.png"
 };
 
 var g_resources = [];
